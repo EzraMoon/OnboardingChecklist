18478d71bef00970035ba906380e9e4921b6d5da
juliapoloSC <123116107+juliapoloSC@users.noreply.github.com>
2023-02-16 15:43:57 -0500

    Understanding js x html

--- onboarding-checklist/src/index.js
+++ onboarding-checklist/src/index.js
@@ -5,3 +5,6 @@ const firstElement = <h1>Hello, World</h1>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(firstElement);
+const subtext = ReactDOM.createRoot(document.getElementById('subtext'));
+subtext.render(<p>testing</p>);
+
