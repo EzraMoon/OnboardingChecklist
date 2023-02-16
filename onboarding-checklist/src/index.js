5114d39177c6ee8b99e34f25030c6eaac960cdd5
juliapoloSC <123116107+juliapoloSC@users.noreply.github.com>
2023-02-16 16:18:09 -0500

    Title, subtext + commenting a lot of code

--- onboarding-checklist/src/index.js
+++ onboarding-checklist/src/index.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const firstElement = <h1>Hello, World</h1>
+/**
+ * Notes:
+ * Functions and class names must be uppercase in order to compile
+ * Prop names must be completely lowercase.
+ */
 
+/**
+ * Class for the title
+ */
+class Title extends React.Component {
+  render() {
+    return <h1>Welcome to the login page!</h1>;
+  }
+}
+
+/**
+ * 
+ * @param {*} props 
+ * @returns 
+ */
+function LoginStatus(props) {
+  return <p>Login status: {props.loggedin}!</p>;
+}
+
+
+// Renders the title of the page, and then adds it to the root div in index.html
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(firstElement);
+root.render(<Title />);
+
+// Renders subtext and adds it to the subtext div in index.html
 const subtext = ReactDOM.createRoot(document.getElementById('subtext'));
-subtext.render(<p>testing</p>);
+subtext.render(<LoginStatus loggedin="Successful"/>);
+
+
 
