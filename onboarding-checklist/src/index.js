b92d3022313bfed0f1b815a06164167e8ff9d86a
juliapoloSC <123116107+juliapoloSC@users.noreply.github.com>
2023-02-17 11:09:18 -0500

    User-entered text functionality

--- onboarding-checklist/src/index.js
+++ onboarding-checklist/src/index.js
@@ -19,6 +19,42 @@ class Title extends React.Component {
 }
 
 /**
+ * Class for user-entry data, specifically
+ * for the username.
+ * Need to figure out how to store this/use it in
+ * the backend.
+ */
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Username: 
+          <input type="text" value={this.state.value} onChange={this.handleChange}/>
+        </label>
+      </form>
+    )
+  }
+}
+
+/**
  * Class that renders the entire login screen
  * Will use it to organize the other classes/componenets
  * so that I can just do root.render(LoginScreen)
@@ -29,6 +65,9 @@ class LoginScreen extends React.Component {
       <div className="loginscreen">
         <div className="title">
           <Title />
+        </div>
+        <div className="textentry">
+          <NameForm/>
         </div>
       </div>
     )
