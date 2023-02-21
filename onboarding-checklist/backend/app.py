3d14705ea922fb4f0062f03203fffa346464aa0d
sarahmosouthernco <smobrien@southernco.com>
2023-02-20 21:11:33 -0500

    changes made by sarah

---
+++ onboarding-checklist/backend/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request
+import getpass
+import flask
+import json
+from flask import Flask, render_template
+from flask_cors import CORS
+
+# calling index.js
+app = Flask(__name__, static_folder= "index.js", static_url_path="/")
+CORS(app, supports_credentials=True)
+
+#incorporating css folder for text font
+@app.route('/')
+def index():
+    return app.send_static_file('index.css')
+
+# validating connection between front end and backend
+@app.route('/validate', methods=['GET', 'POST'])
+def validate():
+    if request.method == 'POST':
+        data = request.get_json()
+        username, password = data
+        #using this until we get database with soco users
+        database = {'sarah.obriennn': "1234", "socoTO": "5678"}
+
+        #if pass and user are in database then returns success or wrong info
+        for i in database.keys():
+            if username == i:
+                while password != database.get(i):
+                    password = ("Wrong password!")
+                return {"success": True}
+        return {"success": False}
+
+#driver code
+if __name__ == '__main__':
+    app.run(debug=True, port=3003)
