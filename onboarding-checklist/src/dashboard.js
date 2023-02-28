4278ef6ba339a879e405f3c097c88f1301d426b8
juliapoloSC <123116107+juliapoloSC@users.noreply.github.com>
2023-02-28 12:25:38 -0500

    Global User Variables Implementation

--- onboarding-checklist/src/dashboard.js
+++ onboarding-checklist/src/dashboard.js
@@ -2,18 +2,19 @@ import React from 'react';
 
 class Dashboard extends React.Component {
 
-    constructor(props) {
+    constructor(props) { // initializes user variables
         super(props)
         this.state = {
-            loginStatus: ""
+            loginStatus: "",
+            user: ""
         }
     }
 
-    handleLogout = () => {
+    handleLogout = () => { // logs the user out
         window.location.pathname = '/'
     }
 
-    fetchUserData() {
+    fetchUserData() { // connects with the backend to receive user data
         console.log("Getting user data from flask...")
         fetch('http://localhost:5000/validate', {
             method: 'GET',
@@ -27,14 +28,20 @@ class Dashboard extends React.Component {
             return r.json()
         }).then(d => {
                 console.log(d)
+                this.setState({user : d.username})
+                console.log(this.state.user)
             })
     }
 
-    render() {
+    componentDidMount() { // Allows us to initialize the states/do stuff before render()
         this.fetchUserData()
+    }
+
+    render() {
         return(
             <div>
                 <h1> Welcome to the Dashboard! </h1>
+                <p>Hi {this.state.user}!</p>
                 <button onClick={this.handleLogout}>Logout</button>
             </div>
         )
